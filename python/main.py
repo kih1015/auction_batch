66a5db983d18fe5243a509f5d6792d227ea59f19
강인화 <rkddlsghk1998@gmail.com>
2025-03-23 06:09:49 +0900

    feat: main 수정

--- python/main.py
+++ python/main.py
@@ -1,9 +1,12 @@
 from fetch_list import fetch_auction_data
+from python.update_expired_auctions import update_expired_auctions
 
 if __name__ == "__main__":
     conditions = [
-        # ("0004601", 0, 14),
+        ("0004601", 0, 14),
         ("0004602", 15, 60)
     ]
     for condition in conditions:
         fetch_auction_data(*condition)
+
+    update_expired_auctions()
